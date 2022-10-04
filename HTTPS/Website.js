4389baaec4448b8ca332ad4cb3ea531ad6d1eab3
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-10-04 20:46:44 +0530

    Creating Website Linking using Node

--- HTTPS/Website.js
+++ HTTPS/Website.js
@@ -1,14 +1,19 @@
 const http = require("http");
+const files = require("fs");
 const port = process.env.PORT || 3000;
 const server = http.createServer((request, response) => {
     response.setHeader("Content-Type", "text/html");
     
     if(request.url == '/'){
-        request.statusCode = 200;
-        response.end("<h1>Hello World</h1><p>This is Ishaan</p>");
+        response.statusCode = 200;
+        const website = files.readFileSync('index.html')
+        response.end(website.toString());
     } else if(request.url == '/about'){
         request.statusCode = 200;
         response.end("<h1>About Hello World</h1><p>Ishaan is what it's all about</p>");
+    } else if (request.url == '/hello'){
+        request.statusCode = 200;
+        response.end("<h1>Hello World</h1><p>This is Ishaan</p>");
     } else {
         // request.IshaanJoshi();
         request.statusCode = 404;
